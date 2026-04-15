4c262bba23fa967dbd1c5d3bf5b522abc65fc313
Tuomas Jalonen <tuomas.a.jalonen@gmail.com>
2026-04-15 04:21:44 -0400

    feat: add database query

--- element.sh
+++ element.sh
@@ -12,19 +12,31 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 GET_INFO() {
+
 # echo message if an argument is not given
 if [[ ! $1 ]]
 then
   echo Please provide an element as an argument.
-# else
-  # get join table
 
-  # get the correct row from the join table using the argument
-  # print results
+else
+  # get correct row from database using the argument
+  ROW=$($PSQL "SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+  FROM elements
+  LEFT JOIN properties ON elements.atomic_number = properties.atomic_number
+  LEFT JOIN types ON properties.type_id = types.type_id
+  WHERE elements.atomic_number::TEXT = '$1'
+  OR name = '$1'
+  OR symbol = '$1';")
 
   # if row is empty print message
-  #echo I could not find that element in the database.
+  if [[ ! $ROW ]]
+  then
+    echo I could not find that element in the database.
+  else
+    echo $ROW
+    # print results
+  fi
 fi
 }
 
-GET_INFO
+GET_INFO $1
